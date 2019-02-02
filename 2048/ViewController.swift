f84bad210f713c98c6dee44f63c14a80d3b09bfd
Yash Patel <patelyash1311@icloud.com>
2019-02-02 09:11:31 -0500

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Yash Patel on 2/2/19.
+//  Copyright © 2019 Yash Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
