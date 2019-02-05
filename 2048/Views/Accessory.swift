c65cf310f57e22b95da70c0d71fc16ddbdfbb47d
Yash Patel <patelyash1311@icloud.com>
2019-02-05 12:55:54 -0500

    Mechanism for score display added

--- 2048/Views/Accessory.swift
+++ 2048/Views/Accessory.swift
@@ -7,3 +7,38 @@
 //
 
 import UIKit
+
+protocol ScoreViewProtocol{
+    func scoreChanged(newScore s:Int)
+}
+
+class ScoreView : UIView, ScoreViewProtocol{
+    var score: Int = 0{
+        didSet{
+            label.text="SCORE:\(score)"
+            
+        }
+    }
+    let defaultFrame = CGRect(x: 0,y: 0,width: 140,height: 140)
+    var label :UILabel
+    
+    init(backgroundColor bgcolor: UIColor,textColor tcolor: UIColor, font :UIFont,radius r: CGFloat){
+        label = UILabel(frame: defaultFrame)
+        label.textAlignment = NSTextAlignment.center
+        super.init(frame: defaultFrame)
+        backgroundColor = bgcolor
+        label.textColor = tcolor
+        label.font = font
+        layer.cornerRadius = r
+        self.addSubview(label)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("NS coading Not Supported")
+    }
+    
+    func scoreChanged(newScore s: Int) {
+        let defaultFrame = CGRect(x: 0, y:0, width: 140, height:140)
+    }
+    
+    
+}
